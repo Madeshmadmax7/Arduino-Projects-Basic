c6a8c29a4c5aa0f8eee2c997765f548860b47425
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2023-12-09 22:03:42 +0530

    Add files via upload

---
+++ sequential led chaser.ino
@@ -0,0 +1,82 @@
+int led1 = 2;
+int led2 = 3;
+int led3 = 4;
+int led4 = 5;
+int led5 = 6;
+int led6 = 7;
+int led7 = 8;
+int led8 = 9;
+int led9 = 10;
+int led10 = 11;
+int led11 = 12;
+int led12 = 13;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
+  pinMode(led9, OUTPUT);
+  pinMode(led10, OUTPUT);
+  pinMode(led11, OUTPUT);
+  pinMode(led12, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led3, HIGH);
+  delay(1000);
+  digitalWrite(led4, HIGH);
+  delay(1000);
+  digitalWrite(led5, HIGH);
+  delay(1000);
+  digitalWrite(led6, HIGH);
+  delay(1000);
+  digitalWrite(led7, HIGH);
+  delay(1000);
+  digitalWrite(led8, HIGH);
+  delay(1000);
+  digitalWrite(led9, HIGH);
+  delay(1000);
+  digitalWrite(led10, HIGH);
+  delay(1000);
+  digitalWrite(led11, HIGH);
+  delay(1000);
+  digitalWrite(led12, HIGH);
+  delay(1000);
+
+  
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  delay(1000);
+  digitalWrite(led3, LOW);
+  delay(1000);
+  digitalWrite(led4, LOW);
+  delay(1000);
+  digitalWrite(led5, LOW);
+  delay(1000);
+  digitalWrite(led6, LOW);
+  delay(1000);
+  digitalWrite(led7, LOW);
+  delay(1000);
+  digitalWrite(led8, LOW);
+  delay(1000);
+  digitalWrite(led9, LOW);
+  delay(1000);
+  digitalWrite(led10, LOW);
+  delay(1000);
+  digitalWrite(led11, LOW);
+  delay(1000);
+  digitalWrite(led12, LOW);
+  delay(1000);
+
+}
